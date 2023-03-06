d77a0f5873490505fe2dd4b0ebe68aeabd72ce1d
Neha Koduru <nkoduru@unomaha.edu>
2023-03-05 22:48:32 -0600

    local-library app || Add feature

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,7 +2,8 @@ import uuid  # Required for unique book instances
 
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -19,6 +20,7 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
+    author_image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
@@ -45,6 +47,7 @@ class Book(models.Model):
                                       'ISBN number</a>')
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    book_image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         """String for representing the Model object."""
@@ -73,6 +76,15 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
+
+    borrower = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """
+         Determines if the book is overdue based on due date and current date.
+         """
+        return bool(self.due_back and date.today() > self.due_back)
 
     class Meta:
         ordering = ['due_back']
