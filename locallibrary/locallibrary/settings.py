b23b284fa445d8bfb49de39f822dea5e6baecdd5
Neha Koduru <nkoduru@unomaha.edu>
2023-03-05 22:51:59 -0600

    local-library app || Update settings

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wvejn%b8%vyra*r^wk_vo2yt3s&^__fibzmkso3nwdp=(274)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
